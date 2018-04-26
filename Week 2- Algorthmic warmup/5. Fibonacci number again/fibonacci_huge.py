028194c29e9c983a46ab97272fef531756acf7a5
MahmadSharaf <36701447+MahmadSharaf@users.noreply.github.com>
2018-04-26 12:44:37 +0200

    Update fibonacci_huge.py

--- Week 2- Algorthmic warmup/5. Fibonacci number again/fibonacci_huge.py
+++ Week 2- Algorthmic warmup/5. Fibonacci number again/fibonacci_huge.py
@@ -14,36 +14,30 @@ def get_fibonacci_huge_naive(n, m):
     return current % m
 def get_pisano_sequence(m):
     '''This Function calculates the Pisano Period'''
+    
     arr=[0,1]
     current=1
     previous=1
     index=2
+    
     while True:
-        new=(arr[index-1]+arr[index-2])
-        arr.append(new%m)
-        previous=current
-        current=arr[index]
-        index+=1
-        if (current==1 and previous==0):
-            arr.pop()
-            arr.pop()
+        new=(arr[index-1]+arr[index-2]) # Calculates the next Fibonaccii number
+        arr.append(new%m)               # Saves the remaining with respect to Value provided
+        previous=current                # Keeps the values of the variables "Previous" and
+        current=arr[index]              # "Current" updated to be used as flags
+        index+=1                        # 
+        if (current==1 and previous==0):# Checks the occurence of 0 followed by 1 which indicates the
+            arr.pop()                   # beginning of a repeatation and therefore remove them of
+            arr.pop()                   # the array
             break
     return arr
 
 
-def calc_fib_fast(n):
-    ar=[]
-    ar.append(0)
-    ar.append(1)
-    for i in range(2,n+1):
-        ar.append(ar[i-1]+ar[i-2])
-    print(ar)
-    return ar[n]
-
 if __name__ == '__main__':
     input = sys.stdin.read();
     n, m = map(int, input.split())
-    Pisano=get_pisano_sequence(int(m))
-    period=len(Pisano)
+    Pisano=get_pisano_sequence(int(m))  # 
+    period=len(Pisano)                  # The length of Pisano Sequence
     # print(Pisano)
-    print(Pisano[n%period])
+    print(Pisano[n%period])             # As we have the sequence we can get Fn mod m by fetching the corresponding index which equals to
+                                        # Fn mod the Pisano period
